9b17e58eac1a72ee6a0e908abd771e5aaacbf013
quan.thanh.vft <quan.thanh.vft@gmail.com>
2023-03-29 21:38:38 +0700

    them cai chuc nang crud cho cac model cua admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Image, Product, Order, OrderDetail, User
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(User)
